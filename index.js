9335e550251a93e83087abf5183f1d7240178723
blff122620 <blff122620@gmail.com>
2017-07-03 14:00:02 +0800

    change title

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(require('express-formidable')({
 // 设置模板全局常量
 app.locals.blog = {
   // title: pkg.name,
-  title: '旭之家-牧马人',
+  title: '@旭 - Hello World',
   description: pkg.description
 };
 
